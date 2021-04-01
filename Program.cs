70c27d53a7ade62c24125db275989bcebe7ded99
Vladimir6545 <kvv.login@gmail.com>
2021-04-01 20:41:12 +0300

    first algorythm

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SomeAlgorithms
+{
+    class Program
+    {
+        public string Test(string text)
+        {
+            List<int> sortList = new List<int>();
+
+            foreach (var item in text)
+            {
+                sortList.Add(Convert.ToInt32(item.ToString()));
+            }
+            sortList.Sort();
+            text = string.Join("", sortList);
+
+            return text;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine(p.Test("9514"));
+        }
+    }
+}
